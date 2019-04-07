a7f99611e2fa36b70ae51d2e3952af7b198fe5c8
hizhangbo <hizhangbo@outlook.com>
2019-04-07 16:09:56 +0800

    本地运行测试

--- src/main/java/top/hizhangbo/App.java
+++ src/main/java/top/hizhangbo/App.java
@@ -2,13 +2,13 @@ package top.hizhangbo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Author: Bob
  * @Datetime: 2019-04-07 14:13
  */
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
